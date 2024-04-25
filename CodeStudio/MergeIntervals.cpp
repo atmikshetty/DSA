deb9e09a2f820337405fc99de45a65c800e0d234
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-04-25 22:38:23 +0530

    Merge all overlapping intervals

---
+++ CodeStudio/MergeIntervals.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
+  // Brute Force O(NogN + 2N)
+  int n = arr.size();
+  vector<vector<int>> ans;
+  for(int i=0;i<n;i++){
+  	int start = arr[i][0];
+  	int end = arr[i][1];
+  	if(!ans.empty() && end <= ans.back()[1]) continue;
+
+  	for(int j=i+1;j<n;j++){
+  		if(arr[j][0] <= end){
+  			end = max(end, arr[j][1]);
+  		}else{
+  			break;
+  		}
+  	}
+  	ans.push_back({start,end});
+  }
+  return ans;
+
+  // O(NlogN + N)
+  int n = arr.size();
+  vector<vector<int>> ans;
+  for (int i = 0; i < n; i++) {
+    if (ans.empty() || arr[i][0] > ans.back()[1]) {
+      ans.push_back(arr[i]);
+    } else {
+      ans.back()[1] = max(ans.back()[1], arr[i][1]);
+    }
+  }
+  return ans;
+}
