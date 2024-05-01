19f2266416801a8ac7ba4be56ed09a5a2b54fcf3
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-05-01 09:40:16 +0530

    Code Studio Binary Search Recursive approach

--- CodeStudio/BinarySearch.cpp
+++ CodeStudio/BinarySearch.cpp
@@ -1,13 +1,28 @@
-int search(vector<int> &nums, int target) {
-    // Binary Search
-    int n = nums.size();
-    int low = 0;
-    int high = n-1;
-    while(low<=high){
-        int mid = (low+high) / 2;
-        if(nums[mid] == target) return mid;
-        else if(target> nums[mid]) low = mid + 1;
-        else high = mid - 1;
+int binarySearch(vector<int> &nums, int target, int low, int high){
+    if(low>high) return-1;
+
+    int mid= (low+high)/2;
+    if(nums[mid] == target) return mid;
+    else if(target>nums[mid]){
+        return binarySearch(nums, target, mid+1,high);
+    }else{
+        return binarySearch(nums, target,low, mid-1);
     }
     return -1;
+}
+
+int search(vector<int> &nums, int target) {
+    // // Binary Search Iterative
+    // int n = nums.size();
+    // int low = 0;
+    // int high = n-1;
+    // while(low<=high){
+    //     int mid = (low+high) / 2;
+    //     if(nums[mid] == target) return mid;
+    //     else if(target> nums[mid]) low = mid + 1;
+    //     else high = mid - 1;
+    // }
+    // return -1;
+
+    return binarySearch(nums, target, 0, nums.size()-1);
 }
