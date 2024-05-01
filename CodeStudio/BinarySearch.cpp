9c4f1637cdf6efe3e459dfe875b40506307357f9
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-05-01 10:19:19 +0530

    Lower bound

--- CodeStudio/BinarySearch.cpp
+++ CodeStudio/BinarySearch.cpp
@@ -2,6 +2,8 @@ int binarySearch(vector<int> &nums, int target, int low, int high){
     if(low>high) return-1;
 
     int mid= (low+high)/2;
+    // also can be written as
+    // int mid  = low + (high-low)/2;
     if(nums[mid] == target) return mid;
     else if(target>nums[mid]){
         return binarySearch(nums, target, mid+1,high);
@@ -25,4 +27,5 @@ int search(vector<int> &nums, int target) {
     // return -1;
 
     return binarySearch(nums, target, 0, nums.size()-1);
+    // O(log2n)
 }
