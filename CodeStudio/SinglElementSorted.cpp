5f59c1885d66d0180427c2d52a4352aaf5a2b5cd
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-05-03 11:17:26 +0530

    Single Element in a Sorted Array

---
+++ CodeStudio/SinglElementSorted.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+int singleNonDuplicate(vector<int> &arr) {
+  // // O(2N)
+  // map<int,int> mpp;
+  // for(int i=0;i<arr.size();i++){
+  // 	mpp[arr[i]]++;
+  // }
+  // for(auto it: mpp){
+  // 	if(it.second == 1) return it.first;
+  // }
+  // return -1;
+
+  // Binary Search
+  int n = arr.size();
+  if (arr[0] != arr[1])
+    return arr[0];
+  if (arr[n-1] != arr[n-2]) return arr[n-1];
+  int low = 1;
+  int high = n - 2;
+    while (low <= high) {
+		int mid = low + (high-low)/2;
+		if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+		
+		if((mid%2) == 1 && arr[mid] == arr[mid-1] || (mid%2) == 0 && arr[mid] == arr[mid+1]){
+			low = mid + 1;
+		}else{
+			high = mid -1;
+		}
+    } 
+	return -1;
+}
