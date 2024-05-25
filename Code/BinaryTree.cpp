d3db1b7ca252f5f6f03411e11f3e12e8019bcce7
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-05-26 01:59:13 +0530

    Binary Trees in C++

---
+++ Code/BinaryTree.cpp
@@ -0,0 +1,20 @@
+struct BinaryTree
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+int main(){
+    struct Node* root = new Node(1);
+
+    root -> left = new Node(2);
+    root -> right = new Node(3);
+
+    print(root)
+}
