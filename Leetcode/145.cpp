1f5fc7839902fc81817349aa8c507653af4369f8
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-06-01 09:20:22 +0530

    145 Iterative

--- Leetcode/145.cpp
+++ Leetcode/145.cpp
@@ -6,23 +6,48 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 
-void Postorder(TreeNode *root, vector<int> &v){
-    if(root == NULL) return;
-    // left right root
-    Postorder(root->left,v);
-    Postorder(root->right,v);
-    v.push_back(root->val);
-}
+    void Postorder(TreeNode* root, vector<int>& v) {
+        if (root == NULL)
+            return;
+        // left right root
+        Postorder(root->left, v);
+        Postorder(root->right, v);
+        v.push_back(root->val);
+    }
 
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> post;
-        Postorder(root, post);
-        return post;
+        // // Recursive
+        // vector<int> post;
+        // Postorder(root, post);
+        // return post;
+
+        // Iterative
+        vector<int> postorder;
+        if(root == NULL) return postorder;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root -> right != NULL){
+                st1.push(root -> right);
+            }
+            if(root -> left != NULL){
+                st1.push(root -> left);
+            }
+        }
+        while(!st2.empty()){
+            postorder.push_back(st2.top() -> val);
+            st2.pop();
+        }
+        return postorder;
     }
 };
