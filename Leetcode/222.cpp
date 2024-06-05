fa3e2ed509a9725fbac3f0ffe6174e76a66e23d9
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-06-05 08:40:44 +0530

    222 Better approach

--- Leetcode/222.cpp
+++ Leetcode/222.cpp
@@ -12,10 +12,38 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        vector<int> ans;
-        inorder(root, ans);
-        return ans.size();
+        // vector<int> ans;
+        // inorder(root, ans);
+        // return ans.size();
+
+        if(root == NULL) return 0;
+
+        int lh = findLeft(root);
+        int rh = findRight(root);
+
+        if(lh == rh) return(1<<lh) - 1;  // 2^n
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
+
+    int findLeft(TreeNode* root){
+        int height = 0;
+        while(root){
+            height++;
+            root = root -> left;
+        }
+        return height;
+    }
+
+    int findRight(TreeNode* root){
+        int height = 0;
+        while(root){
+            height++;
+            root = root -> right;
+        }
+        return height;
+    }
+
     void inorder(TreeNode* root, vector<int> &ans){
         if(root == NULL) return;
         inorder(root->left, ans);
