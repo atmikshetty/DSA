b6c250eb7f60166ee96f3b9cc1ccacb38735e1d7
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-06-11 09:30:20 +0530

    Delete Tail of a Given Linked List

---
+++ Code/DeleteTail.cpp
@@ -0,0 +1,35 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node * deleteTail(Node *head) {
+    // Write your code here.
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp -> next;
+    }
+    return head;
+}
